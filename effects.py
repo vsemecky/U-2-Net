9eef9bae3fa1597fa68a1669d3df580f6cdcd92e
Vojtech Semecky <vojtech@semecky.cz>
2020-10-30 22:56:08 +0100

    Pre/Post processing - in progress...

--- effects.py
+++ effects.py
@@ -46,9 +46,7 @@ class U2Effects:
 
         # Load network
         model_dir = os.path.join(os.getcwd(), 'saved_models', model_name + '.pth')
-        print("model_dir =", model_dir)
-        print("os.getcwd() =", os.getcwd())
-        print("__file__ =", __file__)
+
         if cuda_mode:
             print("CUDA mode")
             self.net.load_state_dict(torch.load(model_dir))
@@ -59,32 +57,8 @@ class U2Effects:
 
         self.net.eval()
 
-    def get_mask(self, image_pil):
-        """ Returns image mask """
-        sample = self.preprocess(image_pil)
-        inputs_test = sample['image'].unsqueeze(0)
-        inputs_test = inputs_test.type(torch.FloatTensor)
-        if self.cuda_mode:
-            inputs_test = Variable(inputs_test.cuda())
-        else:
-            inputs_test = Variable(inputs_test)
-
-        # Predict
-        d1 = self.net(inputs_test)[0]
-        predict = d1[:, 0, :, :]
-        del d1
-        predict = self.norm_pred(predict)  # normalization
-        predict = predict.squeeze()
-        predict_np = predict.cpu().data.numpy()
-
-        # Prepare image mask
-        mask_pil = Image.fromarray(predict_np * 255).convert('RGB')
-        mask_pil = mask_pil.resize(image_pil.size, resample=Image.BILINEAR)  # Resize mask to original image
-
-        return mask_pil
-
-    def get_mask_np(self, image_np):
-        """ Returns image mask """
+    def get_mask(self, image_np):
+        """ Returns image mask as Numpy Array image"""
         sample = self.preprocess(image_np)
         inputs_test = sample['image'].unsqueeze(0)
         inputs_test = inputs_test.type(torch.FloatTensor)
@@ -102,56 +76,55 @@ class U2Effects:
         predict_np = predict.cpu().data.numpy()
 
         # Prepare image mask
+        image_np_size = (image_np.shape[1], image_np.shape[0])
         mask_pil = Image.fromarray(predict_np * 255).convert('RGB')
-        mask_pil = mask_pil.resize(image_np.size, resample=Image.BILINEAR)  # Resize mask to original image
+        mask_pil = mask_pil.resize(image_np_size, resample=Image.BILINEAR)  # Resize mask to original image
 
-        return np.asarray(mask_pil)
+        # return np.asarray(mask_pil)
+        return self.postprocess(mask_pil)
 
-    def get_object(self, image_pil, color="#000000"):
+    @staticmethod
+    def _np2pil(image_np):
+        """ Converts Numpy Array image to PIL Image """
+        return Image.fromarray(np.uint8(image_np)).convert('RGB')
+
+    def get_object(self, image, color="#000000"):
         """ Returns image without background """
-        mask_pil = self.get_mask(image_pil)
+        mask_np = self.get_mask(image)
+        mask_pil = self._np2pil(mask_np)
+        image_pil = self._np2pil(image)
         background_pil = Image.new(mode='RGB', size=image_pil.size, color=color)
         object_pil = Image.composite(image_pil, background_pil, mask_pil.convert("L"))
-        return object_pil
+        return self.postprocess(object_pil)
 
     def blur_background(self, image_pil, blur_radius=5):
         """ Returns image with blured background using Gausian Blur """
         mask_pil = self.get_mask(image_pil)
         background_pil = image_pil.copy().filter(ImageFilter.GaussianBlur(blur_radius))
         object_pil = Image.composite(image_pil, background_pil, mask_pil.convert("L"))
-        return object_pil
+        return self.postprocess(object_pil)
 
     def get_background(self, image_pil, color="#000000"):
         """ Return background without main object """
         mask_pil = self.get_mask(image_pil)
         background_pil = Image.new(mode='RGB', size=image_pil.size, color=color)
         background_pil = Image.composite(background_pil, image_pil, mask_pil.convert("L"))
-        return background_pil
+        return self.postprocess(background_pil)
 
-
-    # Convert to expected output format
-    def postprocess(self, image):
-        # @todo Detekovat format image
-        # @todo Prevest na pozadovany
-
+    @staticmethod
+    def postprocess(image):
+        # If image is PIL.Image, convert to Numpy Array
         if isinstance(image, PIL.Image.Image):
-            print("OUTPUT: pil - converting to opencv")
-            image = np.array(image)[:, :, ::-1].copy()  # PIL image => OpenCv image
-        else:
-            print("OUTPUT: np - using as opencv")
-
+            image = np.asarray(image)
         return image
 
+    # image = Numpy Array
     def preprocess(self, image):
-        # If PIL, convert to Numpy Array
-        if isinstance(image, PIL.Image.Image):
-            print("pil - converting to opencv")
-            image_cv = np.array(image)[:, :, ::-1].copy()  # PIL image => OpenCv image
-        else:
-            print("np - using as opencv")
-            image_cv = image
+        # If image is PIL.Image, convert to Numpy Array
+        # if isinstance(image_cv, PIL.Image.Image):
+        #     image_cv = np.array(image_cv)[:, :, ::-1].copy()  # PIL image => OpenCv image
 
-        label_3 = np.zeros(image_cv.shape)
+        label_3 = np.zeros(image.shape)
         label = np.zeros(label_3.shape[0:2])
 
         if 3 == len(label_3.shape):
@@ -159,15 +132,15 @@ class U2Effects:
         elif 2 == len(label_3.shape):
             label = label_3
 
-        if 3 == len(image_cv.shape) and 2 == len(label.shape):
+        if 3 == len(image.shape) and 2 == len(label.shape):
             label = label[:, :, np.newaxis]
-        elif 2 == len(image_cv.shape) and 2 == len(label.shape):
-            image_cv = image_cv[:, :, np.newaxis]
+        elif 2 == len(image.shape) and 2 == len(label.shape):
+            image = image[:, :, np.newaxis]
             label = label[:, :, np.newaxis]
 
         return self.trans({
             'imidx': np.array([0]),
-            'image': image_cv,
+            'image': image,
             'label': label
         })
 
@@ -178,16 +151,3 @@ class U2Effects:
         mi = torch.min(d)
         dn = (d - mi) / (ma - mi)
         return dn
-
-# # Usage:
-# import effects
-#
-# u2 = U2Effects()
-#
-# file = "/home/vojta/tmp/test.jpg"
-# img = Image.open(file).convert('RGB')
-#
-# u2.get_mask(img).save("/home/vojta/tmp/u2mask.jpg")
-# u2.get_background(img).save("/home/vojta/tmp/u2background.jpg")
-# u2.get_object(img).save("/home/vojta/tmp/u2object.jpg")
-# u2.blur_background(img).save("/home/vojta/tmp/u2zrcadlovka.jpg")
