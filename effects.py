3c7375e187e3ab08d2d5a21ff7fb574d779cf41e
Vojtech Semecky <vojtech@semecky.cz>
2020-10-30 20:12:33 +0100

    Pre/Post processing - in progress...

--- effects.py
+++ effects.py
@@ -1,4 +1,6 @@
 import os
+
+import PIL
 import torch
 from torch.autograd import Variable
 from torchvision import transforms  # , utils
@@ -11,10 +13,29 @@ from model import U2NETP  # small version u2net 4.7 MB
 
 
 class U2Effects:
-    def __init__(self, model_name='u2net', cuda_mode=False):
+
+    # Output image formats
+    FORMATS = [
+        'np',  # Numpy array
+        'pil',  # PIL Image
+    ]
+
+    MODEL_NAMES = [
+        'u2net',  # U2Net (173.6 MB) - slower, but more accurate [Default]
+        'u2netp',  # U2NetP (4.7 MB) - faster, but less accurate
+    ]
+
+    def __init__(self, model_name='u2net', cuda_mode=True, output_format='np'):
         self.model_name = model_name
         self.cuda_mode = cuda_mode and torch.cuda.is_available()  # Fallback to CPU mode, if cuda is not available
         self.trans = transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
+        self.output_format = output_format
+
+        # Validate
+        if output_format not in self.FORMATS:
+            raise AssertionError('Invalid "output_format"', 'Use "np" or "pil"')
+        if model_name not in self.MODEL_NAMES:
+            raise AssertionError('Invalid "model_name"', 'Use "u2net" or "u2netp"')
 
         if model_name == 'u2net':
             print("Model: U2NET (173.6 MB)")
@@ -25,6 +46,9 @@ class U2Effects:
 
         # Load network
         model_dir = os.path.join(os.getcwd(), 'saved_models', model_name + '.pth')
+        print("model_dir =", model_dir)
+        print("os.getcwd() =", os.getcwd())
+        print("__file__ =", __file__)
         if cuda_mode:
             print("CUDA mode")
             self.net.load_state_dict(torch.load(model_dir))
@@ -45,9 +69,8 @@ class U2Effects:
         else:
             inputs_test = Variable(inputs_test)
 
-        d1 = self.net(inputs_test)[0]
-
         # Predict
+        d1 = self.net(inputs_test)[0]
         predict = d1[:, 0, :, :]
         del d1
         predict = self.norm_pred(predict)  # normalization
@@ -59,6 +82,30 @@ class U2Effects:
         mask_pil = mask_pil.resize(image_pil.size, resample=Image.BILINEAR)  # Resize mask to original image
 
         return mask_pil
+
+    def get_mask_np(self, image_np):
+        """ Returns image mask """
+        sample = self.preprocess(image_np)
+        inputs_test = sample['image'].unsqueeze(0)
+        inputs_test = inputs_test.type(torch.FloatTensor)
+        if self.cuda_mode:
+            inputs_test = Variable(inputs_test.cuda())
+        else:
+            inputs_test = Variable(inputs_test)
+
+        # Predict
+        d1 = self.net(inputs_test)[0]
+        predict = d1[:, 0, :, :]
+        del d1
+        predict = self.norm_pred(predict)  # normalization
+        predict = predict.squeeze()
+        predict_np = predict.cpu().data.numpy()
+
+        # Prepare image mask
+        mask_pil = Image.fromarray(predict_np * 255).convert('RGB')
+        mask_pil = mask_pil.resize(image_np.size, resample=Image.BILINEAR)  # Resize mask to original image
+
+        return np.asarray(mask_pil)
 
     def get_object(self, image_pil, color="#000000"):
         """ Returns image without background """
@@ -81,8 +128,28 @@ class U2Effects:
         background_pil = Image.composite(background_pil, image_pil, mask_pil.convert("L"))
         return background_pil
 
-    def preprocess(self, image_pil):
-        image_cv = np.array(image_pil)[:, :, ::-1].copy()  # PIL image => OpenCv image
+
+    # Convert to expected output format
+    def postprocess(self, image):
+        # @todo Detekovat format image
+        # @todo Prevest na pozadovany
+
+        if isinstance(image, PIL.Image.Image):
+            print("OUTPUT: pil - converting to opencv")
+            image = np.array(image)[:, :, ::-1].copy()  # PIL image => OpenCv image
+        else:
+            print("OUTPUT: np - using as opencv")
+
+        return image
+
+    def preprocess(self, image):
+        # If PIL, convert to Numpy Array
+        if isinstance(image, PIL.Image.Image):
+            print("pil - converting to opencv")
+            image_cv = np.array(image)[:, :, ::-1].copy()  # PIL image => OpenCv image
+        else:
+            print("np - using as opencv")
+            image_cv = image
 
         label_3 = np.zeros(image_cv.shape)
         label = np.zeros(label_3.shape[0:2])
